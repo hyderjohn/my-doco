f3f1fb25562ddc3cf394650f11d9ed346b90c23e
Hyder John <hyderjohn@Hyders-MacBook-Pro.local>
2023-04-01 23:19:53 +0530

    Base Setup

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <Button>Header</Button>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cart from '../Components/Cart'
+import Header from '../Components/Header'
+
+const MainPage = () => {
+  return (
+    <div>
+        <Header/>
+        <Cart/>
+    </div>
+  )
+}
+
+export default MainPage
